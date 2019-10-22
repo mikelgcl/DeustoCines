430a31925c49c43a570271693c21ac068a4e62cf
jonmanzanal <jonmanzanal@opendeusto.es>
2019-10-22 19:31:55 +0200

    Base del proyecto(Jon y Mikel)

--- GestionCine/src/Usuario.java
+++ GestionCine/src/Usuario.java
@@ -1,111 +1,53 @@
 
 
 public class Usuario {
-
-	private String nombre;
-	private String contrasenya;
-	private String dni;
 	private String correo;
-	private String numtar;
-	private String contar;
-	private String cadtar;
-	private String dinero;
-	
-	//Constructor
-	public Usuario(String nombre, String contrasenya, String dni, String correo, String numtar, String contar, String cadtar, String dinero) {
+	private String contrasenya;
+	private String nombre;
+	private String apellido;
+	private String tarjetadeusto;
+	public Usuario(String correo, String contrasenya, String nombre, String apellido, String tarjetadeusto) {
 		super();
-		this.nombre = nombre;
-		this.contrasenya = contrasenya;
-		this.dni = dni;
 		this.correo = correo;
-		this.numtar = numtar;
-		this.contar = contar;
-		this.cadtar = cadtar;
-		this.dinero = dinero;
+		this.contrasenya = contrasenya;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.tarjetadeusto = tarjetadeusto;
 	}
-
-
-
-	//Getters y Setters
-	public String getDni() {
-		return dni;
-	}
-
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-
 	public String getCorreo() {
 		return correo;
 	}
-
-
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
-
-	public String getNumtar() {
-		return numtar;
-	}
-
-
-	public void setNumtar(String numtar) {
-		this.numtar = numtar;
-	}
-
-
-	public String getContar() {
-		return contar;
-	}
-
-
-	public void setContar(String contar) {
-		this.contar = contar;
-	}
-
-
-	public String getCadtar() {
-		return cadtar;
-	}
-
-
-	public void setCadtar(String cadtar) {
-		this.cadtar = cadtar;
-	}
-	
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
 	public String getContrasenya() {
 		return contrasenya;
 	}
-
-	public void setContrasenya(String dinero) {
+	public void setContrasenya(String contrasenya) {
 		this.contrasenya = contrasenya;
 	}
-	
-	public String getDinero() {
-		return dinero;
+	public String getNombre() {
+		return nombre;
 	}
-
-	public void setDinero(String dinero) {
-		this.dinero = dinero;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-
-	//To String
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTarjetadeusto() {
+		return tarjetadeusto;
+	}
+	public void setTarjetadeusto(String tarjetadeusto) {
+		this.tarjetadeusto = tarjetadeusto;
+	}
 	@Override
 	public String toString() {
-		return "Usuario [nombre=" + nombre + ", contrasenya=" + contrasenya + ", dni=" + dni + ", correo=" + correo
-				+ ", numtar=" + numtar + ", contar=" + contar + ", cadtar=" + cadtar + "]";
+		return "Usuario [correo=" + correo + ", contrasenya=" + contrasenya + ", nombre=" + nombre + ", apellido="
+				+ apellido + ", tarjetadeusto=" + tarjetadeusto + "]";
 	}
 	
 	
