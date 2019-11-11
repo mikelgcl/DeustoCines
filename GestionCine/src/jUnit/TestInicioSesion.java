82a2a0b6c718681005a21c9e6c80396b95a71b59
Mikel <mikelgonzalez2@gmail.com>
2019-11-11 20:43:55 +0100

    Creados los test de registro e inicio de sesión

---
+++ GestionCine/src/jUnit/TestInicioSesion.java
@@ -0,0 +1,14 @@
+package jUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestInicioSesion {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ GestionCine/src/jUnit/TestRegistro.java
@@ -0,0 +1,14 @@
+package jUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRegistro {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
