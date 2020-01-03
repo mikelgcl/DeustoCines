45cb7928d6c7620e0170ec11631e76b80d559735
Mikel <mikelgonzalez2@gmail.com>
2020-01-03 11:35:51 +0100

    Terminada la ventana de ayuda

--- GestionCine/src/interfaz/VAyuda.java
+++ GestionCine/src/interfaz/VAyuda.java
@@ -18,6 +18,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JTextArea;
 import javax.swing.ImageIcon;
+import javax.swing.JTextPane;
 
 public class VAyuda {
 
@@ -51,9 +52,24 @@ public class VAyuda {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 460, 490);
+		frame.setBounds(100, 100, 460, 591);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel lblPolticaDeProteccin = new JLabel("Política de protección de datos personales");
+		lblPolticaDeProteccin.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPolticaDeProteccin.setBounds(56, 390, 336, 14);
+		frame.getContentPane().add(lblPolticaDeProteccin);
+		
+		JLabel lblPropierdadIntelectualY = new JLabel("Propierdad intelectual y de privacidad");
+		lblPropierdadIntelectualY.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPropierdadIntelectualY.setBounds(56, 194, 336, 14);
+		frame.getContentPane().add(lblPropierdadIntelectualY);
+		
+		JLabel lblNewLabel_1 = new JLabel("Condiciones generales de utilización del website.");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(56, 130, 336, 14);
+		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblCinesDeusto = new JLabel("Cines Deusto");
 		lblCinesDeusto.setForeground(UIManager.getColor("ColorChooser.background"));
@@ -73,43 +89,18 @@ public class VAyuda {
 		});
 		btnAtras.setForeground(Color.WHITE);
 		btnAtras.setBackground(new Color(0, 0, 153));
-		btnAtras.setBounds(46, 401, 95, 23);
+		btnAtras.setBounds(56, 490, 95, 23);
 		frame.getContentPane().add(btnAtras);
-		
-		JLabel lblMakina = new JLabel("EN PROCESO");
-		lblMakina.setFont(new Font("Consolas", Font.BOLD, 50));
-		lblMakina.setBounds(84, 146, 320, 65);
-		frame.getContentPane().add(lblMakina);
-		
-		JLabel lblAyuda = new JLabel("Ayuda:");
-		lblAyuda.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblAyuda.setBounds(46, 118, 58, 14);
-		frame.getContentPane().add(lblAyuda);
-		
-		JLabel label = new JLabel("EN PROCESO");
-		label.setFont(new Font("Consolas", Font.BOLD, 50));
-		label.setBounds(84, 192, 320, 65);
-		frame.getContentPane().add(label);
-		
-		JLabel label_1 = new JLabel("EN PROCESO");
-		label_1.setFont(new Font("Consolas", Font.BOLD, 50));
-		label_1.setBounds(84, 237, 320, 65);
-		frame.getContentPane().add(label_1);
-		
-		JLabel label_2 = new JLabel("EN PROCESO");
-		label_2.setFont(new Font("Consolas", Font.BOLD, 50));
-		label_2.setBounds(84, 282, 320, 65);
-		frame.getContentPane().add(label_2);
-		
-		JLabel label_3 = new JLabel("EN PROCESO");
-		label_3.setFont(new Font("Consolas", Font.BOLD, 50));
-		label_3.setBounds(84, 325, 320, 65);
-		frame.getContentPane().add(label_3);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(VAyuda.class.getResource("/imagenes/fondo1bueno.jpg")));
 		lblNewLabel.setBounds(0, 0, 444, 93);
 		frame.getContentPane().add(lblNewLabel);
+		
+		JTextPane txtpnCondicionesGeneralesDe = new JTextPane();
+		txtpnCondicionesGeneralesDe.setText(" \nCINES DEUSTO es propietaria del dominio y titular de la aplicacion DeustoCines \n\n  \nLa totalidad de esta aplicación: texto, imágenes, marca comercial, logotipo, archivos descargables, botones, combinaciones de colores, así como la estructura, selección, ordenación y presentación de sus contenidos, se encuentra protegida por las leyes españolas e internacionales sobre propie- dad intelectual e industrial. Asimismo, y sin perjuicio de lo anteriormente citado, el contenido de este website también tiene la consideración de programa de ordenador y le es de aplicación toda la normativa española y comunitaria europea vigente sobre la materia. \n\n \nLos criterios y reglas que sigue CINES DEUSTO respecto a la utilización de los datos, que libre y voluntariamente usted facilite a través de nuestra página web, son los que se exponen en la Política de Privacidad." );
+		txtpnCondicionesGeneralesDe.setBounds(54, 127, 346, 345);
+		frame.getContentPane().add(txtpnCondicionesGeneralesDe);
 		
 		frame.setLocationRelativeTo(null);
 	}
