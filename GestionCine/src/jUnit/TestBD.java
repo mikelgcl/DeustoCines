d4090cc126eb4789bc48b5dad0dc602e3fcd368a
jonmanzanal <jonmanzanal@opendeusto.es>
2020-01-08 12:35:37 +0100

    TestBD

---
+++ GestionCine/src/jUnit/TestBD.java
@@ -0,0 +1,39 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.Test;
+
+import baseDeDatos.Pelicula;
+import baseDeDatos.Usuario;
+import baseDeDatos.ZBaseDeDatos;
+
+public class TestBD {
+
+	@Test
+	void testregistro() {
+
+		Usuario u=new Usuario("t", "", "f", "", "");
+		ZBaseDeDatos z=new ZBaseDeDatos();
+		Connection con=z.initBD("DeustoCines");
+		z.insertDatosUsuario(con, u);
+		Usuario usu=z.getusuario(con, "t");
+	    assertEquals("f", usu.getNombre());
+
+	}
+	
+	@Test
+	void testpelicula() {
+		Pelicula p=new Pelicula("g", "", "", "", "", "");
+		ZBaseDeDatos z=new ZBaseDeDatos();
+		Connection con=z.initBD("DeustoCines");
+		z.insertDatosPelicula(con, p);
+		Pelicula prueba=z.getpelicula(con, 0);
+		assertEquals(0, prueba.getCod_Pelicula());
+
+
+	}
+
+}
